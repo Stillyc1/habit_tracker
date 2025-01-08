2505344bf5cc54b874d1936144e7528f2430e235
Alexander_Baranov <Sti_ll@mail.ru>
2025-01-08 20:45:50 +0300

    add model Habit

--- habit_tracker/admin.py
+++ habit_tracker/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
 
-# Register your models here.
+from habit_tracker.models import Habit
+
+
+@admin.register(Habit)
+class HabitAdmin(admin.ModelAdmin):
+    """Отображает модели пользователей в админке"""
+
+    list_display = (
+        "id",
+        "action",
+        "user",
+        "SignPleasantHabit",
+        "RelatedHabit",
+        "Periodicity",
+        "SignPublic",
+    )
+    list_filter = (
+        "id",
+        "user",
+        "SignPleasantHabit",
+        "RelatedHabit",
+        "Periodicity",
+        "SignPublic",
+    )
+    search_fields = (
+        "id",
+        "user",
+        "SignPleasantHabit",
+        "RelatedHabit",
+        "Periodicity",
+        "SignPublic",
+    )
