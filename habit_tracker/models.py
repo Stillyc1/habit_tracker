084d5588141e8b12f4a913e682a874de5bb6cebd
Alexander_Baranov <Sti_ll@mail.ru>
2025-01-12 19:46:55 +0300

    add urls project

--- habit_tracker/models.py
+++ habit_tracker/models.py
@@ -7,17 +7,17 @@ class Habit(models.Model):
     time = models.DateTimeField(verbose_name='Время выполнения привычки',
                                 help_text='Введите время в формате (01.01.2001 12:30)')
     action = models.CharField(max_length=510, verbose_name='Действие привычки', help_text='Введите действие привычки')
-    SignPleasantHabit = models.BooleanField(verbose_name='Признак приятной привычки')
-    RelatedHabit = models.ForeignKey('habit_tracker.Habit', on_delete=models.SET_NULL, null=True, blank=True,
-                                     verbose_name='Связанная привычка')
-    Periodicity = models.PositiveIntegerField(verbose_name='Периодичность привычки. в днях',
+    sign_pleasant_habit = models.BooleanField(verbose_name='Признак приятной привычки')
+    related_habit = models.ForeignKey('habit_tracker.Habit', on_delete=models.SET_NULL, null=True, blank=True,
+                                      verbose_name='Связанная привычка')
+    periodicity = models.PositiveIntegerField(verbose_name='Периодичность привычки. в днях',
                                               help_text='Выберите период привычки в днях.', default=1,
                                               choices=[(1, 1), (2, 2), (3, 3), (4, 4), (5, 5), (6, 6), (7, 7)])
-    Reward = models.CharField(max_length=255, verbose_name='Вознаграждение', help_text='Вознаграждение', null=True,
+    reward = models.CharField(max_length=255, verbose_name='Вознаграждение', help_text='Вознаграждение', null=True,
                               blank=True)
-    TimeToComplete = models.PositiveIntegerField(verbose_name='Время на выполнение (не больше 120сек)',
-                                                 help_text='Введите время выполнения привычки (не больше 120сек)')
-    SignPublic = models.BooleanField(verbose_name='Признак публичной привычки')
+    time_to_complete = models.PositiveIntegerField(verbose_name='Время на выполнение (не больше 120сек)',
+                                                   help_text='Введите время выполнения привычки (не больше 120сек)')
+    sign_public = models.BooleanField(verbose_name='Признак публичной привычки')
 
     def __str__(self):
         return f"{self.action}"
